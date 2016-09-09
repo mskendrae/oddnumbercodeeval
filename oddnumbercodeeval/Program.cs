10f8efbb4a1c7cdd77547b2156f111c147c0ca55
Kendra Egert <mskendrae@gmail.com>
2016-09-08 22:50:02 -0400

    first commit

---
+++ oddnumbercodeeval/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace oddnumbercodeeval
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //print out odd numbers from 1 thhrough 99 inclusive
+            //start at 1, make condition lt or = 99, add 2 after each loop
+            int counter;
+            for (counter = 1; counter <= 99; counter += 2)
+            {
+                System.Console.WriteLine(counter);
+
+            }
+        }
+    }
+}
